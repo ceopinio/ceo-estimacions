49d3bfabd3aa9cbcea7d2062950a20f32e8b63f5
Gonzalo Rivero <griverorz@gmail.com>
2022-04-25 21:27:10 -0400

    Use general config instead of setup

--- src/setup.R
+++
@@ -1,9 +0,0 @@
-DTA_FOLDER <- "../dta"
-RAW_DTA_FOLDER <- file.path(DTA_FOLDER, "raw-dta")
-IMG_FOLDER <- "../img"
-TXT_FOLDER <- "../txt"
-
-
-assetize <- function(x) {
-  return(file.path(DTA_FOLDER, x))
-}

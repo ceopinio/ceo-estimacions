885abbee4f56869e41d465b8c745403ba8d41d8f
Gonzalo Rivero <griverorz@gmail.com>
2022-06-14 19:13:12 -0400

    Remove conda

--- ansible/install-conda.sh
+++
@@ -1,6 +0,0 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-mkdir -p /root/.conda
-bash Miniconda3-latest-Linux-x86_64.sh -b
-rm -f Miniconda3-latest-Linux-x86_64.sh
-echo PATH="/root/miniconda3/bin":$PATH >> .bashrc
-exec bash 

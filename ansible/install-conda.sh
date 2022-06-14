d0bf9c0097bcbf2b4ab75b5bff398651a4c210e1
Gonzalo Rivero <griverorz@gmail.com>
2022-06-14 18:57:31 -0400

    Install conda in remote

--- ansible/install-conda.sh
+++ ansible/install-conda.sh
@@ -1,5 +1,5 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-mkdir /root/.conda
+mkdir -p /root/.conda
 bash Miniconda3-latest-Linux-x86_64.sh -b
 rm -f Miniconda3-latest-Linux-x86_64.sh
 echo PATH="/root/miniconda3/bin":$PATH >> .bashrc

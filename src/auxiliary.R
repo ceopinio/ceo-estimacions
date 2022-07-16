ec4b4750a3da1e3a4a64d273ec196eab3dbaf44a
Gonzalo Rivero <griveroz@gmail.com>
2022-07-16 13:04:30 -0400

    Remove unused script

--- src/auxiliary.R
+++
@@ -1,9 +0,0 @@
-## Eliminate accents and punctuation so that they can be used as
-## regular R factors
-get_model <- function(location, basename) {
-  mfit <- readRDS(file.path(location, paste0(basename, ".RDS")))
-  m <- xgb.load(file.path(location, paste0(basename, ".xgb")))
-  mfit$finalModel$handle <- m$handle
-  mfit$finalModel$raw <- m$raw
-  return(mfit)
-}

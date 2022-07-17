58b0cce3183340e7e9144ee6aacfc093997913d9
Gonzalo Rivero <griveroz@gmail.com>
2022-07-17 13:28:25 -0400

    Use direct question about vote

--- src/data-cleaning.R
+++ src/data-cleaning.R
@@ -30,18 +30,16 @@ bop <- bop[sample(nrow(bop)),]
 
 bop <- bop |>
   mutate(intention=case_when(INT_PARLAMENT_VOT_R %in% ## Vote intention
-                               c(1, 3, 4, 6, 10, 18, 21, 23) ~ INT_PARLAMENT_VOT_R,
-                             INT_PARLAMENT_VOT %in%
-                               c(80, 93, 94) ~ 80, ## "Altres partits" (with "nul" and "en blanc") 
-                             INT_PARLAMENT_VOT_R > 94 ~ NA_real_),
+                               c(1, 3, 4, 6, 10, 18, 21, 23, 80) ~ INT_PARLAMENT_VOT_R,
+                             TRUE ~ NA_real_),
          recall=case_when(REC_PARLAMENT_VOT_R < 93 ~ REC_PARLAMENT_VOT_R,
                           REC_PARLAMENT_VOT_R %in% c(93, 94) ~ 80, ## "Altres partits" (with "nul" and "en blanc")
                           REC_PARLAMENT_VOT_R > 96 ~ 98), ## Vote recall
-         abstention=case_when(INT_PARLAMENT_VOT_R %in% 96 ~ "Will.not.vote",
-                              INT_PARLAMENT_VOT_R %in% c(98, 99) ~ NA_character_,
-                              TRUE ~ "Will.vote"), ## Stated abstention
-         simpatia=case_when(SIMPATIA_PARTIT_AGRUPADA_R < 93 ~ SIMPATIA_PARTIT_AGRUPADA_R,
-                            SIMPATIA_PARTIT_AGRUPADA_R > 93 ~ NA_real_), ## Stated proximity
+         abstention=case_when(INT_PARLAMENT_PART %in% c(1, 2) ~ "Will.not.vote",
+                              INT_PARLAMENT_PART %in% c(4, 5) ~ "Will.vote",
+                              TRUE ~ NA_character_), ## Stated abstention
+         simpatia=case_when(SIMPATIA_PARTIT_AGRUPADA_R <= 95 ~ SIMPATIA_PARTIT_AGRUPADA_R,
+                            SIMPATIA_PARTIT_AGRUPADA_R > 95 ~ NA_real_), ## Stated proximity
          across(c("IDEOL_0_10",
                   "ESP_CAT_0_10",
                   "RISCOS"), ~ if_else(.x >= 98,
@@ -57,7 +55,6 @@ bop <- bop |>
                   "SIT_LAB",
                   "ACTITUD_INDEPENDENCIA",
                   "RELACIONS_CAT_ESP",
-                  "LLENGUA_PRIMERA",
                   "SIT_ECO_CAT",
                   "SIT_ECO_CAT_RETROSPECTIVA",
                   "SIT_ECO_CAT_PROSPECTIVA",                                    
@@ -70,8 +67,7 @@ bop <- bop |>
                   "INF_POL_RADIO_FREQ",                  
                   "INF_POL_TV_FREQ",
                   "INF_POL_XARXES_FREQ",
-                  "PART_ELECCIONS",
-                  "INT_PARLAMENT_PART"), ~ if_else(.x >= 98,
+                  "PART_ELECCIONS"), ~ if_else(.x >= 98,
                                                NA_integer_,
                                                as.integer(.x))), ## Categorical variables
          across(starts_with("CONEIX_"), ~ if_else(.x >= 98,
@@ -81,6 +77,9 @@ bop <- bop |>
                         perl=TRUE), ~ if_else(.x >= 98,
                                               NA_real_,
                                               as.numeric(.x))), ## Evaluation
+         LLENGUA_PRIMERA=case_when(LLENGUA_PRIMERA %in% c(1, 2, 3, 80) ~ LLENGUA_PRIMERA,
+                                   LLENGUA_PRIMERA == 4 ~ 80, ## Not enough cases for Aranes
+                                   TRUE ~ NA_real_),
          polinfo_tv=case_when(INF_POL_TV_CANAL %in% c(1, 3, 5, 6, 9, 10, 11) ~ INF_POL_TV_CANAL,
                               INF_POL_TV_CANAL %in% c(80:84, 2, 4, 12, 13, 95) ~ 80, ## Altres
                               TRUE ~ NA_real_), ## Political information TV

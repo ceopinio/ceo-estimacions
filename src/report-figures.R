8efba9cff27141d1929df887f592e0f01a790e18
Agustín Blanco <106583756+AgusBB99@users.noreply.github.com>
2022-10-27 08:42:42 +0200

    Updated with new figures

--- src/report-figures.R
+++ src/report-figures.R
@@ -3,13 +3,13 @@
 ## Creates figures for the final report
 
 library(yaml)
-library(haven)
 library(ggplot2)
 library(forcats)
 library(dplyr)
+library(readr)
+library(haven)
 library(escons)
 library(tidyr)
-library(ggsankey)
 library(MESS)
 
 ## ---------------------------------------- 
@@ -18,8 +18,23 @@ library(MESS)
 list2env(read_yaml("./config/config.yaml"), envir=globalenv())
 
 bop <- read_sav(file.path(RAW_DTA_FOLDER, "Microdades anonimitzades 1031.sav"))
+
 evotes <- readRDS(file.path(DTA_FOLDER, "estimated-vote-share.RDS"))
 eseats <- readRDS(file.path(DTA_FOLDER, "seats.RDS"))
+
+past_results <- read_csv2(file.path(RAW_DTA_FOLDER, "past_results_plots.csv"))
+past_seats <- past_results %>% select(party, past_seats = seats_2021)
+past_vote <- past_results %>% select(party, past_vote = vote_2021)
+
+p_recall <- readRDS(file.path(DTA_FOLDER, "p_recall.RDS"))
+p_behavior <- readRDS(file.path(DTA_FOLDER, "individual-behavior.RDS"))
+p_voting <- readRDS(file.path(DTA_FOLDER, "predicted-partychoice.RDS"))
+p_transfer <- merge(p_recall, p_behavior, by="id")
+p_transfer <- merge(p_transfer, p_voting, by="id")
+
+## Font (Only for Windows users)
+
+windowsFonts(Arial=windowsFont("Arial"))
 
 ## Party colors
 
@@ -41,7 +56,6 @@ prettify_party_names <- function(x) {
   
   return(x)
 }
-
 
 ## ---------------------------------------- 
 ## Estimate vote shares
@@ -66,61 +80,89 @@ evotes <- evotes |>
 evotes <- evotes[!evotes$party %in% c("Altres"),
                  c("party", "weighted", "propvote", "lb", "ub")]
 
-## Sort levels by results
-sorted_levels <- evotes$party[order(evotes$propvote, decreasing=TRUE)]
-evotes$party <- factor(evotes$party, levels=as.character(sorted_levels))
-
 ## Sort colors to match levels in data
 evotes_party_color <- party_color[levels(evotes$party)]
 evotes_party_color_alpha <- party_color_alpha[levels(evotes$party)]
 
+## Join Past Results
+
+evotes <- evotes %>% left_join(past_vote, by = "party")
+
+## Sort levels by results
+sorted_levels <- evotes$party[order(evotes$propvote, decreasing=TRUE)]
+evotes$party <- factor(evotes$party, levels=as.character(sorted_levels))
+
 ## Report plot
+
 p <- ggplot(evotes,
-            aes(party, propvote, fill=party))
-pq <- p + geom_col(width=0.7,
-           show.legend=FALSE) +
-  geom_hline(aes(yintercept=0)) +
-  geom_crossbar(aes(x=party,
-                    y=propvote,
-                    ymin=lb,
-                    ymax=ub,
-                    fill=party,
-                    color=party),
-                width=0.7,
-                alpha=0.5,
-                linetype=3,
-                fatten=0) +
-  geom_text(size=3,
-            aes(party,
-                label=round(ub, digits=0),
-                y=ub),
-            vjust=-.5) +
-  geom_text(size=3,
-            aes(party,
-                label=round(lb, digits=0),
-                y=lb),
-            vjust=1.5) +
+            aes(propvote, party,  fill=party))
+
+pq <- p + geom_col(width = 0.5,
+                   position = position_nudge(y = 0.12)) +
+  geom_crossbar(aes(xmin = lb, 
+                    xmax = ub,
+                    fill = party,
+                    color = party),
+                width = 0.5,
+                alpha = 0.5, 
+                linetype = 3, 
+                fatten = 0,
+                position = position_nudge(y = 0.125)) +
+  geom_col(aes(x = past_vote,
+               y = party,
+               fill = party),
+           alpha = 0.5,
+           width = 0.3,
+           position = position_nudge(y = -0.3)) +
+  geom_vline(aes(xintercept = 0)) +
+  geom_text(aes(past_vote,
+                label = past_vote, 
+                y = party),
+            hjust = -.1,
+            vjust = 1.6,
+            fontface = "italic",
+            size = 3) +
+  geom_text(aes(lb, 
+                label=round(lb, digits=0), 
+                y = party),
+            hjust = 1.3,
+            vjust = 0.1,
+            fontface = "bold") +
+  geom_text(aes(ub, 
+                label=round(ub, digits=0), 
+                y = party),
+            hjust = -0.3,
+            vjust = 0.1,
+            fontface = "bold") +
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(limits = c(0,33), 
+                     expand = c(0, 0)) +
   scale_fill_manual(values=evotes_party_color_alpha) +
   scale_color_manual(values=evotes_party_color) +
-  scale_y_continuous(limits=c(0, 30),
-                     labels=c("0", "10", "20", "30")) +
   theme_minimal() +
-  theme(legend.position="none",
-        panel.grid.minor.x=element_blank(),
-        panel.grid.major.x=element_blank(),
-        panel.grid.minor.y=element_blank(),
-        plot.background=element_rect(fill="white",
-                                     colour="white"),
-        plot.margin=margin(0.5, 0.5, 0, 0.5, "cm"),
-        axis.title.y=element_text(margin=margin(0, 0.5, 0, 0, "cm"),
-                                  face="italic"),
-        text=element_text(face="bold")) +
-  labs(x="",
-       y="Percentatge de vot (IC95%)")
+  theme(legend.position = "none",
+        panel.grid.major.y = element_blank(),
+        plot.background =  element_rect(fill = 'white', 
+                                        colour = 'white'),
+        axis.title.x = element_text(margin = margin(0.5,0,0,0, "cm")),
+        plot.margin = margin(0.5,0.5,0,0.5, "cm"),
+        text = element_text(family = "Arial", color  = "black"),
+        plot.title = element_text(margin = margin(0.25,0,0.25,0, "cm"),
+                                  face = "bold", 
+                                  color = "black"),
+        plot.subtitle = element_text(margin = margin(0,0,0.5,0, "cm"),
+                                     color="#ACACAC",
+                                     face = "italic"),
+        plot.title.position = "plot",
+        plot.caption.position = "plot") +
+  labs(title = "Percentatge de Vot vàlid (± 95%CI)", 
+       subtitle = "vs Resultats 2021",
+       x= "",
+       y = "")
+pq
 
-ggsave(file.path(IMG_FOLDER, "figevots.png"), pq,
-       units="in", width=8, height=8, dpi=300)
-
+ggsave(file.path(IMG_FOLDER, "figevots_parlament.png"), pq,
+       units="cm", width=15, height=10, dpi=300)
 
 ## ---------------------------------------- 
 ## Estimated seat distribution
@@ -129,59 +171,190 @@ ggsave(file.path(IMG_FOLDER, "figevots.png"), pq,
 eseats$party <- as.factor(eseats$party)
 eseats$party <- prettify_party_names(eseats$party)
 
+## Sort colors to match levels in data
+eseats_party_color <- party_color[levels(eseats$party)]
+eseats_party_color_alpha <- party_color_alpha[levels(eseats$party)]
+
+## Join Past Results
+
+eseats <- eseats %>% left_join(past_seats, by = "party")
+
 ## Sort levels by results
 sorted_levels <- eseats$party[order(eseats$hi95,
                                     decreasing=TRUE)]
 eseats$party <- factor(eseats$party,
                        levels=sorted_levels)
 
-## Sort colors to match levels in data
-eseats_party_color <- party_color[levels(eseats$party)]
-eseats_party_color_alpha <- party_color_alpha[levels(eseats$party)]
-
 ## Report plot
+
 p <- ggplot(eseats,
-            aes(party, median, fill=party))
+            aes(median, party, fill=party))
 
 pq <- p +
-  geom_col(width=0.7, show.legend=FALSE) +
-  geom_hline(aes(yintercept = 0)) +
-  geom_crossbar(aes(x=party,
-                    y=median,
-                    ymin=lo05,
-                    ymax=hi95,
-                    fill=party,
-                    color=party),
-                width=0.7,
-                alpha=0.5,
-                linetype=3,
-                fatten=0) +
-  geom_text(aes(party,
-                label=hi95,
-                y=hi95),
-            vjust=-.5) +
-  geom_text(aes(party,
-                label=lo05,
-                y=lo05),
-            vjust=1.5) +
-  scale_fill_manual(values=eseats_party_color) +
-  scale_color_manual(values=eseats_party_color_alpha) +
-  scale_y_continuous(limits=c(0, 42),
-                     labels=c("0", "10", "20", "30", "40")) +
+  geom_col(aes(fill = party),
+           width = 0.5,
+           position = position_nudge(y = 0.12)) +
+  geom_crossbar(aes(xmin = lo05, 
+                    xmax = hi95,
+                    fill = party,
+                    color = party),
+                width = 0.5,
+                alpha = 0.5, 
+                linetype = 3, 
+                fatten = 0,
+                position = position_nudge(y = 0.125)) +
+  geom_col(aes(x = past_seats,
+               y = party,
+               fill = party),
+           alpha = 0.5,
+           width = 0.3,
+           position = position_nudge(y = -0.3)) +
+  geom_vline(aes(xintercept = 0)) +
+  geom_text(aes(past_seats, 
+                label = past_seats, 
+                y = party),
+            hjust = -.1,
+            vjust = 1.6,
+            fontface = "italic",
+            size = 3) +
+  geom_text(aes(lo05, 
+                label = lo05, 
+                y = party),
+            hjust = 1.3,
+            vjust = 0.1,
+            fontface = "bold") +
+  geom_text(aes(hi95, 
+                label = hi95, 
+                y = party),
+            hjust = -0.3,
+            vjust = 0.1,
+            fontface = "bold") +
+  scale_fill_manual(values=evotes_party_color_alpha) +
+  scale_color_manual(values=evotes_party_color) +
+  scale_y_discrete(limits = rev) +
+  scale_x_continuous(limits = c(0,44), expand = c(0, 0)) +
+  theme_minimal() +
+  theme(legend.position = "none",
+        panel.grid.major.y = element_blank(),
+        plot.background =  element_rect(fill = 'white', 
+                                        colour = 'white'),
+        axis.title.x = element_text(margin = margin(0.5,0,0,0, "cm")),
+        plot.margin = margin(0.5,0.5,0,0.5, "cm"),
+        text = element_text(family = "Arial", color  = "black"),
+        plot.title = element_text(margin = margin(0.25,0,0.25,0, "cm"),
+                                  face = "bold", 
+                                  color = "black"),
+        plot.subtitle = element_text(margin = margin(0,0,0.5,0, "cm"),
+                                     color="#ACACAC",
+                                     face = "italic"),
+        plot.title.position = "plot",
+        plot.caption.position = "plot") +
+  labs(title = "Escons (± 95% CI)", 
+       subtitle = "vs Resultats 2021",
+       x= "",
+       y = "")
+
+pq
+
+ggsave(file.path(IMG_FOLDER, "figescons_parlament.png"), pq,
+       units="cm", width=15, height=10, dpi=300)
+
+## ---------------------------------------- 
+
+## Heatmapf of transference
+
+hmap_p <- p_transfer %>%
+  group_by(p_intention, p_recall, .drop = FALSE) %>%
+  summarize(n=length(p_recall)) %>%
+  ungroup() %>%
+  complete(p_recall,
+           p_intention,
+           fill=list(n=0, freq=0)) %>%
+  group_by(p_recall) %>%
+  mutate(proportion=(n / sum(n))*100) %>%
+  mutate(proportion=round_percent(proportion, decimals = 0)) 
+
+hmap_p <- hmap_p%>%
+  mutate(p_intention=case_when(p_intention == "PSCPSOE" ~ "PSC",
+                               p_intention == "En.Comu.Podem" ~ "ECP",
+                               p_intention == "Junts.per.Catalunya" ~ "Junts",
+                               p_intention == "No.votaria" ~ "BAI",
+                               p_intention == "PP" ~ "PP",
+                               p_intention == "ERC" ~ "ERC",
+                               p_intention == "Cs" ~ "Cs",
+                               p_intention == "CUP" ~ "CUP",
+                               p_intention == "Vox" ~ "Vox",
+                               p_intention == "Altres" ~ "Altres"),
+         p_recall=case_when(p_recall == "PSCPSOE" ~ "PSC",
+                            p_recall == "PP" ~ "PP",
+                            p_recall == "ERC" ~ "ERC",
+                            p_recall == "Cs" ~ "Cs",
+                            p_recall == "CUP" ~ "CUP",
+                            p_recall == "Vox" ~ "Vox",
+                            p_recall == "En.Comu.Podem" ~ "ECP",
+                            p_recall == "Junts.per.Catalunya" ~ "Junts",
+                            p_recall == "Altres.partits" ~ "Altres",
+                            p_recall == "No.va.votar" ~ "BAI"))
+
+
+## Order of parties in plot
+partits_level <- c("PSC",
+                   "ERC",
+                   "Junts",
+                   "Vox",
+                   "CUP",
+                   "ECP",
+                   "Cs",
+                   "PP",
+                   "Altres",
+                   "BAI")
+
+## Plot heatmap
+
+p <- ggplot(hmap_p)
+
+pq <- p +
+  geom_tile(aes(fct_relevel(p_intention, partits_level),
+                fct_relevel(p_recall, partits_level),
+                fill=p_recall,
+                alpha=proportion),
+            color="white",
+            size=1) +
+  geom_text(aes(p_intention,
+                p_recall,
+                label=proportion),
+            color="white",
+            size=4,
+            fontface="bold") +
+  scale_y_discrete(limits=rev) +
+  scale_x_discrete(position="top") +
+  scale_fill_manual(values=as.vector(c("#AEAEAE", #Altres
+                                       "#AEAEAE", #BAI
+                                       party_color_alpha["Cs"],
+                                       party_color_alpha["CUP"],
+                                       party_color_alpha["ECP"],
+                                       party_color_alpha["ERC"],
+                                       party_color_alpha["Junts"],
+                                       party_color_alpha["PP"],
+                                       party_color_alpha["PSC"],
+                                       party_color_alpha["Vox"]))) +
+  scale_alpha_continuous(limits=c(0, 10),
+                         range=c(0.3, 1)) +
   theme_minimal() +
   theme(legend.position="none",
         panel.grid.minor.x=element_blank(),
         panel.grid.major.x=element_blank(),
         panel.grid.minor.y=element_blank(),
+        panel.grid.major.y=element_blank(),
         plot.background=element_rect(fill="white",
                                      colour="white"),
-        plot.margin=margin(0.5, 0.5, 0, 0.5, "cm"),
-        axis.title.y=element_text(margin=margin(0, 0.5, 0, 0, "cm"),
-                                    face="italic"),
-        text=element_text(face="bold")) +
-  labs(x="", 
-       y="Escons (Percentils 5 i 95 de simulacions)")
-
-ggsave(file.path(IMG_FOLDER, "figescons.png"), pq,
-       units="in", width=8, height=8, dpi=300)
-
+        plot.margin=margin(0.5, 0.5, 0.5, 0.5, "cm"),
+        axis.title.x.top = element_text(face = "bold",
+                                        margin=margin(0, 0, 0.5, 0, "cm")),
+        axis.title.y = element_text(face = "bold",
+                                    margin=margin(0, 0.5, 0, 0, "cm"))) +
+  labs(x="Estimació de vot 2022",
+       y="Record de vot 2021")
+pq
+ggsave(file.path(IMG_FOLDER, "heatmap_parlament.png"), pq,
+       units="cm", width=15, height=10,  dpi=300)

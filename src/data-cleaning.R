5edb4e0bfd9efdd291a654c8058d8935611def72
Centre d'Estudis d'Opinió <96474299+ceopinio@users.noreply.github.com>
2022-11-09 12:44:20 +0100

    update data-cleaning bop 3a onada 2022

--- src/data-cleaning.R
+++ src/data-cleaning.R
@@ -14,7 +14,7 @@ library(stringi)
 ## Read in data and configuration
 
 list2env(read_yaml("config/config.yaml"), envir=globalenv())
-bop <- read_sav(file.path(RAW_DTA_FOLDER, "Microdades anonimitzades 1031.sav"))
+bop <- read_sav(file.path(RAW_DTA_FOLDER, "Microdades anonimitzades 1035.sav"))
 
 ## ---------------------------------------- 
 ## Assign ID to data
@@ -32,22 +32,23 @@ bop <- bop |>
   mutate(intention=case_when(INT_PARLAMENT_VOT_R %in% ## Vote intention
                                c(1, 3, 4, 6, 10, 18, 21, 23, 80) ~ INT_PARLAMENT_VOT_R,
                              TRUE ~ NA_real_),
-         recall=case_when(REC_PARLAMENT_VOT_R < 93 ~ REC_PARLAMENT_VOT_R,
+         recall=case_when(REC_PARLAMENT_VOT_R < 93 ~ REC_PARLAMENT_VOT_R, ## Vot ultimes eleccions
                           REC_PARLAMENT_VOT_R %in% c(93, 94) ~ 80, ## "Altres partits" (with "nul" and "en blanc")
                           REC_PARLAMENT_VOT_R > 96 ~ 98), ## Vote recall
          abstention=case_when(INT_PARLAMENT_PART %in% c(1, 2, 4, 5) ~ INT_PARLAMENT_PART,
                               TRUE ~ NA_real_), ## Stated abstention
-         simpatia=case_when(SIMPATIA_PARTIT_AGRUPADA_R <= 95 ~ SIMPATIA_PARTIT_AGRUPADA_R,
-                            SIMPATIA_PARTIT_AGRUPADA_R > 95 ~ NA_real_), ## Stated proximity
-         across(c("IDEOL_0_10",
-                  "ESP_CAT_0_10",
+         simpatia = case_when(SIMPATIA_PARTIT_R %in% c(1, 3, 4, 6, 10, 18, 21, 23, 80, 95) ~ SIMPATIA_PARTIT_R,
+                              SIMPATIA_PARTIT_R %in% c(98, 99) ~ NA_real_,
+                              TRUE ~ 80), ## Stated proximity
+         SIMPATIA_PARTIT_PROPER_R = case_when(SIMPATIA_PARTIT_PROPER %in% c(1, 3, 4, 6, 10, 18, 21, 23, 80, 95) ~ SIMPATIA_PARTIT_PROPER,
+                                              is.na(SIMPATIA_PARTIT_PROPER) ~ NA_real_, 
+                                              SIMPATIA_PARTIT_PROPER %in% c(98, 99) ~ NA_real_,
+                                              TRUE ~ 80), ## No simpatia partit, quin més proximitat
+         across(c("IDEOL_0_10", ## Extrema esquerra - extrema dreta
+                  "ESP_CAT_0_10", ## Maxim espanyolisme - maxim catalanisme
                   "RISCOS"), ~ if_else(.x >= 98,
                                        NA_real_,
                                        as.numeric(.x))), ## Spatial variables
-         across(matches("SIMPATIA_[A-Z]{1,}_0_10|SIMPATIA_VOTANTS_[A-Z]{1,}_0_10"),
-                ~ if_else(.x >= 98,
-                          NA_integer_,
-                          as.integer(.x))), ## Proximity/affect by party
          across(c("EDAT_GR",
                   "RELIGIO_FREQ",
                   "SIT_LAB",
@@ -61,42 +62,44 @@ bop <- bop |>
                   "SIT_POL_ESP",
                   "SATIS_DEMOCRACIA",
                   "INTERES_POL_PUBLICS",
-                  "INF_POL_DIARI_FREQ",
-                  "INF_POL_RADIO_FREQ",                  
-                  "INF_POL_TV_FREQ",
-                  "INF_POL_XARXES_FREQ",
+                  "INF_POL_MON_FREQ",
                   "PART_ELECCIONS"), ~ if_else(.x >= 98,
                                                NA_integer_,
                                                as.integer(.x))), ## Categorical variables
          across(starts_with("CONEIX_"), ~ if_else(.x >= 98,
                                                   NA_integer_,
-                                                  as.integer(.x))), ## Knowledge
+                                                  as.integer(.x))), ## Coneixement liders politics
          across(matches("VAL_[A-Z]{1}_[A-Z]{1,}",
                         perl=TRUE), ~ if_else(.x >= 98,
                                               NA_real_,
-                                              as.numeric(.x))), ## Evaluation
-         across(c("SIT_POL_CAT", "SIT_ECO_ESP", "SIT_POL_ESP"),
+                                              as.numeric(.x))), ## Valoracio liders polítics
+         across(c("VAL_GOV_CAT", "VAL_GOV_ESP"),
+                ~ if_else( .x >= 98, NA_real_, as.numeric(.x))), ## Valoracio governs
+         across(c("CONFI_POL_CAT", "CONFI_POL_ESP"),
+                ~ if_else( .x >= 98, NA_real_, as.numeric(.x))), ## Confianca politics    
+         across(c("EFICACIA_EXT_1", "EFICACIA_EXT_2", "EFICACIA_INT_1", "EFICACIA_INT_2"),
+                ~ case_when( .x %in% c(1, 2) ~ .x,
+                         TRUE ~ NA_real_)),
+         across(c("SIT_ECO_CAT", "SIT_POL_CAT", "SIT_ECO_ESP", "SIT_POL_ESP"),
                 ~ case_when(.x %in% c(1, 2) ~ 1,
                             .x == 3 ~ 0,
                             .x %in% c(4, 5) ~ -1,
                             TRUE ~ NA_real_)),
          GENERE=case_when(GENERE %in% c(1, 2) ~ GENERE,
                           TRUE ~ NA_real_),
+         RELACIONS_CAT_ESP = case_when( RELACIONS_CAT_ESP %in% c(1, 2, 3, 4) ~  as.numeric(RELACIONS_CAT_ESP),
+                                   TRUE ~ NA_real_),    
+         RELACIONS_CAT_ESP_1_5 = case_when( RELACIONS_CAT_ESP_1_5 %in% c(1, 2, 3, 4, 5) ~  as.numeric(RELACIONS_CAT_ESP_1_5),
+                                       TRUE ~ NA_real_),
          LLENGUA_PRIMERA=case_when(LLENGUA_PRIMERA %in% c(1, 2, 3, 80) ~ LLENGUA_PRIMERA,
                                    LLENGUA_PRIMERA == 4 ~ 80, ## Not enough cases for Aranes
                                    TRUE ~ NA_real_),
-         polinfo_tv=case_when(INF_POL_TV_CANAL %in% c(1, 3, 5, 6, 9, 10, 11) ~ INF_POL_TV_CANAL,
-                              INF_POL_TV_CANAL %in% c(80:84, 2, 4, 12, 13, 95) ~ 80, ## Altres
-                              TRUE ~ NA_real_), ## Political information TV
-         polinfo_radio=case_when(INF_POL_RADIO_EMISORA %in% c(1:11) ~ INF_POL_RADIO_EMISORA,
-                                 INF_POL_RADIO_EMISORA %in% c(80:84, 12, 95) ~ 80, ## Altres
-                                 TRUE ~ NA_real_), ## Political information radio
          estudis_1_5=case_when(ESTUDIS_1_15 %in% c(1, 2, 3) ~ 1L, ## Less than primary
                                ESTUDIS_1_15 %in% c(4) ~ 2L, ## Secondary
                                ESTUDIS_1_15 %in% c(5, 6) ~ 3L, ## High School
                                ESTUDIS_1_15 %in% c(7, 8, 9) ~ 4L, ## Some College
-                               ESTUDIS_1_15 %in% c(10:80) ~ 5L, ## Above
-                               ESTUDIS_1_15 >= 98 ~ NA_integer_),
+                               ESTUDIS_1_15 %in% c(10:15) ~ 5L, ## Above
+                               ESTUDIS_1_15 >= 80 ~ NA_integer_),
          ingressos_1_5=case_when(INGRESSOS_1_15 %in% c(1:5) ~ 1L, ## Less than 1000,
                                  INGRESSOS_1_15 %in% c(6:8) ~ 2L, ## Less than 2000,
                                  INGRESSOS_1_15 %in% c(9:10) ~ 3L, ## Less than 3000
@@ -108,6 +111,7 @@ bop <- bop |>
          "simpatia",
          "abstention",
          # Spatial variables
+         "SIMPATIA_PARTIT_PROPER_R",
          "IDEOL_0_10",
          "ESP_CAT_0_10",
          "RISCOS",
@@ -115,8 +119,16 @@ bop <- bop |>
          "GENERE",
          "EDAT_GR",
          "RELIGIO_FREQ",
+         "EFICACIA_EXT_1", 
+         "EFICACIA_EXT_2", 
+         "EFICACIA_INT_1", 
+         "EFICACIA_INT_2",
          "SIT_LAB",
+         "VAL_GOV_CAT", 
+         "VAL_GOV_ESP",
          "ACTITUD_INDEPENDENCIA",
+         "RELACIONS_CAT_ESP",
+         "RELACIONS_CAT_ESP_1_5",
          "RELACIONS_CAT_ESP",
          "LLENGUA_PRIMERA",
          "SIT_ECO_CAT",
@@ -127,20 +139,14 @@ bop <- bop |>
          "SIT_POL_ESP",
          "SATIS_DEMOCRACIA",
          "INTERES_POL_PUBLICS",
-         "INF_POL_DIARI_FREQ",
-         "INF_POL_RADIO_FREQ",                  
-         "INF_POL_TV_FREQ",
-         "INF_POL_XARXES_FREQ",
+         "INF_POL_MON_FREQ",
          "PART_ELECCIONS",
-         "polinfo_tv",
-         "polinfo_radio",
          "estudis_1_5",
          "ingressos_1_5",
          "PROVINCIA",
          "HABITAT",
          # Knowledge and evaluation
          starts_with("CONEIX_"),
-         matches("SIMPATIA_[A-Z]{1,}_0_10|SIMPATIA_VOTANTS_[A-Z]{1,}_0_10"),
          matches("VAL_[A-Z]{1}_[A-Z]{1,}", perl=TRUE)) |>
   mutate(across(-matches("0_10|VAL_[A-Z]{1}_[A-Z]{1,}",
                          perl=TRUE), ~ as_factor(.))) |> ## Most vars are factors

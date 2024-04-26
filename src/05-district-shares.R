7320e36560b2ef444b5909ce5dbd7b4efcc09c95
Centre d'Estudis d'Opinió <96474299+ceopinio@users.noreply.github.com>
2024-04-26 12:16:32 +0200

    Enquesta telefònica electoral

--- src/05-district-shares.R
+++ src/05-district-shares.R
@@ -15,38 +15,47 @@ options(mc.cores = parallel::detectCores() - 1)
 
 ## ---------------------------------------- 
 ## Read in data and configuration
-
 list2env(read_yaml("./config/config.yaml"), envir=globalenv())
-bop <- readRDS(file.path(DTA_FOLDER, "clean-bop.RDS"))
+df <- readRDS(file.path(DTA_FOLDER, "clean-df.RDS"))
 
 p_intention <- readRDS(file.path(DTA_FOLDER, "individual-behavior.RDS"))
 
-past_results <- read_delim(file.path(RAW_DTA_FOLDER, "results-2021.csv"),  delim = ";", escape_double = FALSE, trim_ws = TRUE)
+past_results <- readr::read_delim(file.path(RAW_DTA_FOLDER, "results-2021.csv"),  delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 ## Merge data
-bop <- merge(bop, p_intention, by = "id")
+df <- merge(df, p_intention, by = "id")
 
-bop <- bop[!bop$p_intention %in% c("No.votaria"), ] ## Only interested in vote shares
-bop <- droplevels(bop)
+df <- df[!df$p_intention %in% c("No.votaria"), ] ## Only interested in vote shares
+df <- droplevels(df)
 
 ## ---------------------------------------- 
 ## Set priors for vote share in each district
 
 ## Calculate relation between results in district and results in Catalonia
-sresults <- prop.table(xtabs(weight ~ p_intention, data=bop))
+sresults <- prop.table(xtabs(weight ~ p_intention, data=df))
 
 ## Shares in previous election
 results <- past_results |>
   filter(code != 8000) |>
-  mutate(code=case_when(code %in% c(93, 94, 80, 6) ~ #Nul, Blanc, Altres, Ciudadanos
+  mutate(code=case_when(code %in% c(93, 94, 80) ~ #Nul, Blanc, Altres
                           80,
                         code == 22 ~ 18,
                         TRUE ~ code),
-         code=factor(code, levels(bop$p_intention))) |>
+         code=factor(code, levels(df$p_intention))) |>
   group_by(provincia, code) |>
   summarize(votes=sum(votes)) |>
+  ungroup() |>
   as.data.frame()
-results$code <- factor(results$code[results$code != "6"])
+
+## We add a non-informative prior for Aliança Catalana, because it is a new party
+## Just like if they had a 2% of the valid votes in each district
+results_AC <- results |>
+  summarise(total = sum(votes), .by = provincia) |>
+  mutate(code = 25, votes = round(total*0.02)) |>
+  select(-total)
+
+results <- rbind(results, results_AC)
+results$code <- factor(results$code)
 
 results <- xtabs(votes ~ provincia + code, data=results)
 catshare <- prop.table(colSums(results)) ## Catalonia results
@@ -59,10 +68,11 @@ P <- apply(cfactors, 2, \(x) x*sresults/sum(x*sresults))
 ## model
 fit <- dshare(p_intention ~ provincia,
               weights=weight,
-              data=bop,
+              data=df,
               priors=P,
-              sd=.025,
-              chains=3)
+              sd=.025, # This sets the importance of the priors (higher sd, less informative priors)
+              chains=3,
+              iter=2000) # If no convergence, increase iterations
 
 ## ---------------------------------------- 
 ## Save estimates in simulation format
@@ -76,4 +86,3 @@ dimnames(pestimates) <- dimnames(t(cfactors))
 ## ---------------------------------------- 
 ## Save data
 saveRDS(pestimates, file.path(DTA_FOLDER, "vote-share-district.RDS"))
-

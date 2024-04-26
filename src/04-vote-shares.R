e3e0fa0ef528f25b5c648d34043ffb5688d0fec5
Centre d'Estudis d'Opinió <96474299+ceopinio@users.noreply.github.com>
2024-04-26 12:16:05 +0200

    Enquesta telefònica electoral

--- src/04-vote-shares.R
+++ src/04-vote-shares.R
@@ -16,56 +16,56 @@ library(ggplot2); theme_set(theme_bw())
 ## Read in data and configuration
 
 list2env(read_yaml("./config/config.yaml"), envir=globalenv())
-bop <- readRDS(file.path(DTA_FOLDER, "clean-bop.RDS"))
+df <- readRDS(file.path(DTA_FOLDER, "clean-df.RDS"))
 
 ## Recall weights
 recall_weights <- readRDS(file.path(DTA_FOLDER, "weight.RDS"))
 
 ## Predicted behavior
-p_partychoice <- readRDS(file.path(DTA_FOLDER, "predicted-partychoice.RDS"))
+p_partychoice <- readRDS(file.path(DTA_FOLDER, "predicted-partychoice.RDS")) # Atenció
 p_voting <- readRDS(file.path(DTA_FOLDER, "predicted-voting.RDS"))
 thr_voting <- readRDS(file.path(DTA_FOLDER, "thr-predicted-voting.RDS"))
 
 ## Join all results
-bop <- merge(bop, recall_weights, by="id")
-bop <- merge(bop, p_partychoice, by="id")
-bop <- merge(bop, p_voting, by="id") ## Probability of voting
+df <- merge(df, recall_weights, by="id")
+df <- merge(df, p_partychoice, by="id")
+df <- merge(df, p_voting, by="id") ## Probability of voting
 
 ## ---------------------------------------- 
 ## Consolidate results
 
-bop$abstention_twofactor <- as.factor(ifelse(bop$abstention %in% c(3, 4), "Will.vote", "Will.not.vote"))
-bop$abstention_twofactor[is.na(bop$abstention)] <- NA
+df$abstention_twofactor <- as.factor(ifelse(df$abstention %in% c(3, 4), "Will.vote", "Will.not.vote"))
+df$abstention_twofactor[is.na(df$abstention)] <- NA
 
 ## Predicted behavior defaults to reported behavior
-bop$p_intention <- bop$intention
+df$p_intention <- df$intention
 ## If they did not report a party choice, assign the predicted 
-bop$p_intention[is.na(bop$intention)] <- as.factor( sub("^p", "", bop$p_partychoice[is.na(bop$intention)]) )
+df$p_intention[is.na(df$intention)] <- as.factor( sub("^p", "", df$p_partychoice[is.na(df$intention)]) )
 ## If they declare they will not vote, use that behavior
-levels(bop$p_intention) <- c(levels(bop$p_intention), "No.votaria")
-bop$p_intention[bop$abstention_twofactor == "Will.not.vote"] <- "No.votaria"
-## Assign to voting all respondents with low predicted probability
-## of voting (relative to cutoff)
-bop$p_intention[(bop$p_voting < thr_voting) & is.na(bop$intention)] <- "No.votaria"
+levels(df$p_intention) <- c(levels(df$p_intention), "No.votaria")
+df$p_intention[df$abstention_twofactor == "Will.not.vote"] <- "No.votaria"
+## Assign to nonvoters all respondents with low predicted probability of voting (relative to cutoff)
+## We allow flipping in this case of telephone survey, to not overrate participation
+df$p_intention[(df$p_voting < thr_voting)] <- "No.votaria"
 
-bop$p_intention <- droplevels(bop$p_intention)
+df$p_intention <- droplevels(df$p_intention)
 
 ## Save results 
-saveRDS(data.frame("id"=bop$id,
-                   "p_intention"=bop$p_intention,
-                   "weight"=bop$weight),
+saveRDS(data.frame("id"=df$id,
+                   "p_intention"=df$p_intention,
+                   "weight"=df$weight),
         file.path(DTA_FOLDER, "individual-behavior.RDS"))
 
 ## ---------------------------------------- 
 ## Estimated vote shares
 
 ## Unweighted estimates
-estimates <- prop.table(xtabs(~ p_intention, data=bop))*100
+estimates <- prop.table(xtabs(~ p_intention, data=df))*100
 estimates <- as.data.frame(estimates)
 names(estimates)[names(estimates) == "Freq"] <- "unweighted"
 
 ## Weighted estimates
-westimates <- prop.table(xtabs(weight ~ p_intention, data=bop))*100
+westimates <- prop.table(xtabs(weight ~ p_intention, data=df))*100
 westimates <- as.data.frame(westimates)
 names(westimates)[names(westimates) == "Freq"] <- "weighted"
 
@@ -84,24 +84,24 @@ saveRDS(estimates, file.path(DTA_FOLDER, "estimated-vote-share.RDS"))
 
 turnout <- seq(.4, 1, by=0.01) ## True turnout will trail expected turnout
 n_notvoting <- (1 - turnout) * 
-  (nrow(bop) - sum(bop$abstention_twofactor == "Will.not.vote", na.rm=TRUE))
+  (nrow(df) - sum(df$abstention_twofactor == "Will.not.vote", na.rm=TRUE))
 
-vote <- bop$intention
-vote[is.na(bop$intention)] <- as.factor( sub("^p", "", bop$p_partychoice[is.na(bop$intention)]) )
+vote <- df$intention
+vote[is.na(df$intention)] <- as.factor( sub("^p", "", df$p_partychoice[is.na(df$intention)]) )
 vote <- rep(list(as.character(vote)), length(turnout))
 
 ## Predicted as abstainers (among those who have reported they will vote)
 pabstainers <- lapply(round(n_notvoting),
-                      \(x) sort(bop$p_voting[bop$abstention_twofactor == "Will.vote"])[1:x])
+                      \(x) sort(df$p_voting[df$abstention_twofactor == "Will.vote"])[1:x])
 last_prob_pabstainers <- lapply(pabstainers, max)
 
 for (i in seq_along(last_prob_pabstainers)) {
-  pabstainers[[i]] <- which(bop$p_voting < last_prob_pabstainers[[i]] &
-                              bop$abstention_twofactor == "Will.vote")
+  pabstainers[[i]] <- which(df$p_voting < last_prob_pabstainers[[i]] &
+                              df$abstention_twofactor == "Will.vote")
 }
 
 ## Declared as abstainers
-dabstainers <- rep(list(which(bop$abstention_twofactor == "Will.not.vote")),
+dabstainers <- rep(list(which(df$abstention_twofactor == "Will.not.vote")),
                    length(turnout))
 abstainers <- mapply(function(x, y) unique(c(x, y)),
                      x=pabstainers,
@@ -109,14 +109,31 @@ abstainers <- mapply(function(x, y) unique(c(x, y)),
 
 for (i in seq_along(turnout)) vote[[i]][abstainers[[i]]] <- "No.votaria"
 
-sim <- as.data.frame(sapply(vote,
-                            \(x) prop.table(xtabs(bop$weight ~ x,
-                                                  subset=x != "No.votaria"))))
+sim <- sapply(vote, \(x) prop.table(xtabs(df$weight ~ x,
+                                                  subset=x != "No.votaria")))
+
+# # Iterate through each element in the sim list
+for (i in seq_along(sim)) {
+   # Check if there is no element with name "6"
+   if (!"6" %in% names(sim[[i]])) {
+     # Create an element with name "6" and value 0
+     sim[[i]]["6"] <- 0
+   }
+ }
+ 
+ for (i in seq_along(sim)) {
+   # Order the element based on its names
+   sim[[i]] <- sim[[i]][order(names(sim[[i]]))]
+ }
+ 
+ sim <- do.call(cbind, sim)
+
 eturnout <- 1 - sapply(vote,
                        \(x) prop.table(xtabs(weight ~ x == "No.votaria",
-                                             data=bop))["TRUE"]) ## Actual turnout
+                                             data=df))["TRUE"]) ## Actual turnout
 
 names(sim) <- paste0("p", turnout*100)
+sim <- as.data.frame(sim)
 sim$party <- rownames(sim)
 
 sim <- reshape(sim,
@@ -125,7 +142,7 @@ sim <- reshape(sim,
                v.names="share",
                timevar="p")
 
-sim$ep <- rep(eturnout, each=9)
+sim$ep <- rep(eturnout, each=10)
 
 ## Relation between turnout levels and probability of abstaining
 pt_turnout <- cbind.data.frame(turnout, eturnout)
@@ -142,13 +159,19 @@ ggsave(file.path(IMG_FOLDER, "pvoting-turnout.pdf"), pq)
 
 ## Because we are keeping everyone who declared to abstain with their
 ## reported choice, the maximum turnout is the declared turnout
+## Add threshold line
+eturnout_line <- pt_turnout %>%
+  filter(abs(last_prob_pabstainers - thr_voting) == 
+             min(abs(last_prob_pabstainers - thr_voting)))
+
 p <- ggplot(sim, aes(x=ep, y=share, group=party, colour=party))
 pq <- p + geom_line() +
+  geom_vline(xintercept = eturnout_line$eturnout, linetype=2) +
   labs(title="Effect of simulated turnout on vote share",
        x="Turnout rate",
        y="Vote share") +
   lims(x=c(0, 1),
-       y=c(0, .3)) +
+       y=c(0, .4)) +
   scale_color_discrete("Party")
  
 ggsave(file.path(IMG_FOLDER, "simulation-voting.pdf"), pq)
